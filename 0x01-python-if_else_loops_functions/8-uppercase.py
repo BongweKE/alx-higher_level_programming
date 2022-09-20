f24729ff7857c66ace19c0a042ccf9cd5b5f98f6
BongweKE <bongwe.obi@gmail.com>
2022-09-20 21:54:29 +0000

    Function to convert string to uppercase v0.1 pycodestyle correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@ def islower(c):
     to_ascii = ord(c)
     return to_ascii in range(97, 123)
 
+
 def uppercase(str):
     for c in str:
         print("{}".format(chr(ord(c) - 32)) if islower(c) else c, end="")
