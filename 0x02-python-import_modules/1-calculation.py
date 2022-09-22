d64e16945b96be45d16d22b57eb8c50245a39983
BongweKE <bongwe.obi@gmail.com>
2022-09-22 21:36:17 +0000

    Calculator functions f strings

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+
+def main():
+    """The main function
+    Args: None
+    Returns: Only prints
+    """
+    a = 10
+    b = 5
+    print(f"{a} + {b} = {add(a, b)}")
+    print(f"{a} - {b} = {sub(a, b)}")
+    print(f"{a} * {b} = {mul(a, b)}")
+    print(f"{a} / {b} = {div(a, b)}")
+
+
+if __name__ == "__main__":
+    main()
