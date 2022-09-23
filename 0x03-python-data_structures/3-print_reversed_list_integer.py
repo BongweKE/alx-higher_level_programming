dd140318b9392af7fe0c721f4af03409fc9a087a
BongweKE <bongwe.obi@gmail.com>
2022-09-23 11:53:49 +0000

    Reverse a list in py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    """function that prints all integers of a list, in reverse order.
+
+    Requirements:
+        Format: one integer per line.
+        1. You are not allowed to import any module
+        2. You can assume that the list only contains integers
+        3. You are not allowed to cast integers into strings
+        4. You have to use str.format() to print integers
+
+    """
+    temp = my_list[:]
+    temp.reverse()
+    for i in temp:
+        print("{:d}".format(i))
