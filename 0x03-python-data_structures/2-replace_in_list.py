8f7dc58373c7d8a984d510d5d06ff48ceab82744
BongweKE <bongwe.obi@gmail.com>
2022-09-23 11:15:12 +0000

    Function to replace an element in a py list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    """function that replaces an element of a list at a specific position
+    Requirements:
+        a) If idx is negative, the function should not modify anything,
+        and returns the original list
+        b) If idx is out of range (> of number of element in my_list),
+        the function should not modify anything, and returns the original list
+        c) You are not allowed to import any module
+        d) You are not allowed to use try/except
+
+    Return val:
+        Modified or original list
+    """
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list
