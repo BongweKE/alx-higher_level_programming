63ea6b9010a4a7f4790ba9256dd0888f35a0cad9
BongweKE <bongwe.obi@gmail.com>
2022-09-26 07:35:51 +0000

    Print a matrix in py using a oneliner

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """A function that prints a matrix of integers.
+
+    Requirements:
+    1. You are not allowed to import any module
+    2. You can assume that the list only contains integers
+    3. You are not allowed to cast integers into strings
+    4. You have to use str.format() to print integers
+    """
+    for row in matrix:
+        for item in row:
+            print("{:s}{:d}".format(
+                '' if row.index(item) == 0 else ', ', item),
+                  end='')
+        print()
