d8336cf0187eccd395ba4a5fecf264107a17c7c8
BongweKE <bongwe.obi@gmail.com>
2022-09-26 08:33:22 +0000

    Using tuples to inspect length and first char of a sentence in py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """A function that returns a tuple with the length of a
+    string and its first character.
+
+    Requirements:
+        If the sentence is empty, the first character should be equal to None
+        You are not allowed to import any module
+
+    Return:
+        (length, first char or none)
+    """
+    if len(sentence) == 0:
+        return(0, None)
+    else:
+        return(len(sentence), sentence[0])
