071aaec9320294ecc6be3ef53cccb7aff850d532
BongweKE <bongwe.obi@gmail.com>
2022-09-27 07:15:20 +0000

    Handle list=None in reverse list printer function, ensure that wc -m is 0

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -3,4 +3,5 @@ print_reversed_list_integer = __import__(
     '3-print_reversed_list_integer').print_reversed_list_integer
 
 my_list = None
+
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -12,7 +12,6 @@ def print_reversed_list_integer(my_list=[]):
     """
 
     if my_list is None:
-        print(None)
         return None
 
     for i in reversed(my_list):
