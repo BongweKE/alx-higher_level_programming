2b0d76425c23ef0bab2e3396a1ea270c6cf25765
BongweKE <bongwe.obi@gmail.com>
2022-09-28 19:21:02 +0000

    Use intersection in sets

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    """A function that adds all unique integers in a list
+    (only once for each integer).
+
+    Requirements:
+        You are not allowed to import any module
+
+    Return:
+        Sum
+    """
     if my_list is None:
         return 0
     return sum(set(my_list))
