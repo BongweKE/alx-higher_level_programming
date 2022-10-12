0afdcca6e5504c0e4ac96d20d93bbd42cc0e5509
BongweKE <bongwe.obi@gmail.com>
2022-10-12 10:45:10 +0000

    Create function to raise a name error

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """a function that raises a name exception with a message.
+
+    Requirements:
+    You are not allowed to import any module
+    """
+
+    raise NameError(message)
