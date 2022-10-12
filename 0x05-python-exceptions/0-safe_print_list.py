8c2cbe5d4dbaeabc1a1270290f570e84bdf7715e
BongweKE <bongwe.obi@gmail.com>
2022-10-12 08:57:03 +0000

    Update length constraing while printing a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     j = 0
     try:
         for i in my_list:
-            if j <= x and x > 0:
+            if j < x and x > 0:
                 print(f"{i}", end='')
                 j += 1
             else:
