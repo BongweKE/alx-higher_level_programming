6d32fd48d89032fef0be8b376f9af83231bb121d
BongweKE <bongwe.obi@gmail.com>
2022-10-12 09:01:55 +0000

    Change position of new line printer

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
                 j += 1
             else:
                 return j
-            print()
+        print()
     except BaseException as err:
         raise
     return j
