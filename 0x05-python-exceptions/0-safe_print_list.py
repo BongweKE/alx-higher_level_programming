72e935a6a3f00812a8c6ee2cfa2646eac59ce2ea
BongweKE <bongwe.obi@gmail.com>
2022-10-12 08:36:29 +0000

    Print a list safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    try:
+        for i in my_list:
+            if j <= x and x > 0:
+                print(f"{i}", end='')
+                j += 1
+            else:
+                return j
+            print()
+    except BaseException as err:
+        raise
+    return j
