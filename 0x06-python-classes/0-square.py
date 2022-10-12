d9b8cbc6c0c818db05583875d104418e04a22be0
BongweKE <bongwe.obi@gmail.com>
2022-10-12 18:53:16 +0000

    Create base files and complete task one with proper docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""a module for an empty square class
 
+Example:
+    This module will be imported to another class and do nothing
+    $ ./0-main.py
+    <class '0-square.Square'>
+    {}
+"""
 class Square:
     """an empty class Square that defines a square
     """
