46ecafc3c1ef4b91986a1d1f5f05cbff3e7bbab9
BongweKE <bongwe.obi@gmail.com>
2022-10-12 10:41:10 +0000

    Create a function to raise a type exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def raise_exception():
+    """A function that raises a type exception.
+
+    Requirements:
+    You are not allowed to import any module
+    """
+    raise TypeError
