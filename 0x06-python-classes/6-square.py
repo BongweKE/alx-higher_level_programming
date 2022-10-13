334d4438bc6c94ba4ae584e3117d2b1903d7be6e
BongweKE <bongwe.obi@gmail.com>
2022-10-13 09:12:31 +0000

    Create a method for Square instances for them to be printed using position specifiers

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,28 +40,50 @@ Example:
     --
 
     --
+
+    $ ./6-main.py | tr " " "_" | cat -e
+    ###$
+    ###$
+    ###$
+    --$
+    _###$
+    _###$
+    _###$
+    --$
+    ___###$
+    ___###$
+    ___###$
+    --$
     """
 
 
 class Square:
-    """a class Square that defines a mathematical square
-    """
+    """a class Square that defines a mathematical square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialization of Class Square
 
         Args:
             __size (int): length or width should be equal and private
+            __position (int, int): an tuple of 2 positive integers
+
         Note:
             size is expected to be an integer >= 0
+            position must be a tuple of two positive integers
         """
 
         self.size = size
+        self.position = position
 
     @property
     def size(self):
-        """int: the lenth or width """
+        """int: the lenth or width"""
         return self.__size
+
+    @property
+    def position(self):
+        """(int, int): 2d position of our square"""
+        return self.__position
 
     @size.setter
     def size(self, size):
@@ -72,6 +94,16 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    @position.setter
+    def position(self, position):
+        if type(position[0]) is not int or type(position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if position < (0, 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        self.__position = position
 
     def area(self):
         """Calculate the area of asquare based on size
@@ -95,7 +127,8 @@ class Square:
             print()
         else:
             _pr = [
-                ['#' for i in range(self.__size)]
+                [' ' for s in range(self.__position[0])]
+                + ['#' for i in range(self.__size)]
                 for j in range(self.__size)
             ]
 
