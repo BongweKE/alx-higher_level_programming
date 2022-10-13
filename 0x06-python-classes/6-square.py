12223e6077aa2637f2f17d74dc5293d092aafa42
BongweKE <bongwe.obi@gmail.com>
2022-10-13 12:32:22 +0000

    Correct indentation for continuation line

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -98,8 +98,8 @@ class Square:
     @position.setter
     def position(self, position):
         if len(position) < 2 or\
-        type(position[0]) is not int or type(position[1]) is not int\
-        or position[0] < 0 or position[1] < 0:
+           type(position[0]) is not int or type(position[1]) is not int\
+           or position[0] < 0 or position[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = position
