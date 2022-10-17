0b2f0b429f9796b7429b12f0155627faef134595
BongweKE <bongwe.obi@gmail.com>
2022-10-17 20:05:57 +0000

    Add perimeter and area methods to the rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+""" A module containing definition of a real rectangle
+
+Note:
+    It can calculate Area and Perimeter
+"""
+
+
+class Rectangle:
+    """A realistic mathematical rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Method to initialize the Rectangle
+        Attrs:
+            width (int): The width of the rectangle
+            height (int): The rectangle height
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """int: the width of our rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """int: the height of our rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        else:
+            self.__height = value
+
+    def area(self):
+        """A method to calculate rectangle area
+
+        Return: height * width
+        """
+        return self.__height * self.width
+
+    def perimeter(self):
+        """A method to calculate perimeter
+
+        Return: perimeter
+        """
+        return (self.__height * 2) + (self.width * 2)
