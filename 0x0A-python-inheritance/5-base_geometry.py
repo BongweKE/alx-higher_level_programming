3314ca774c678232b0718bec44ab02d4f3c8b11c
BongweKE <bongwe.obi@gmail.com>
2022-10-27 08:37:47 +0000

    Create an empty BaseGeometry class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module for empty class BaseGeometry()"""
+
+
+class BaseGeometry:
+    """An empty class"""
+    def __init__(self):
+        """ a method to initialize the class instance"""
+        pass
