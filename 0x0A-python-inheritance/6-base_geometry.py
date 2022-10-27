92f9499b968878220d2f04ebc6b0542d06955d30
BongweKE <bongwe.obi@gmail.com>
2022-10-27 19:20:26 +0000

    Create a BaseGeometry with area funciton

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Module for class BaseGeometry() which is based on module 5-base_geometry"""
+
+
+class BaseGeometry:
+    """A child class of BaseGeometry from Module 5-base_geometry"""
+    def __init__(self):
+        """ a method to initialize the class instance"""
+        pass
+
+    def area(self):
+        """ a method to calculate area of a geometrical figure"""
+        raise Exception("area() is not implemented")
