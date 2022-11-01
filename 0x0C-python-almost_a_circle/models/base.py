8b8b5705880865e160490a1dc9dd43ba23303af2
BongweKE <bongwe.obi@gmail.com>
2022-11-01 16:54:25 +0000

    Convert to_json_string to a static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,6 +19,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """A function to return a json representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
