8392215a031bb329fa9d6f362310d1b076c3ffb0
BongweKE <bongwe.obi@gmail.com>
2022-11-01 20:01:55 +0000

    Create unittests for Square class instances

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Unit tests for instances of Square"""
 import unittest
+import os
 from models.square import Square
 from models.base import Base
 from models.rectangle import Rectangle
@@ -37,6 +38,20 @@ class TestsForSquare(unittest.TestCase):
             Square(4, 4, 7, 7).to_dictionary(),
             {'id': 7, 'x': 4, 'y': 7, 'size': 4})
 
+        # test create method
+        self.assertEqual(
+            str(Square.create(**{ 'id': 89 })),
+            '[Square] (89) 0/0 - 1')
+        self.assertEqual(
+            str(Square.create(**{ 'id': 89, 'size': 1 })),
+            '[Square] (89) 0/0 - 1')
+        self.assertEqual(
+            str(Square.create(**{ 'id': 89, 'size': 1, 'x': 2 })),
+            '[Square] (89) 2/0 - 1')
+        self.assertEqual(
+            str(Square.create(**{ 'id': 89, 'size': 1, 'x': 2, 'y': 3 })),
+            '[Square] (89) 2/3 - 1')
+
     def test_square_errors(self):
         """Test TypeErrors and ValueErrors"""
         with self.assertRaises(TypeError):
@@ -61,6 +76,35 @@ class TestsForSquare(unittest.TestCase):
     def test_sub_class(self):
         """Confirnm that Rectangle is subclass of Base"""
         self.assertEqual(issubclass(Square, Rectangle), True)
+
+    def tests_that_open_file(self):
+        """Test methods that save json to the file"""
+        Square.save_to_file([])
+        with open("Square.json") as f:
+            string_read = f.read()
+            self.assertEqual(string_read, '[]')
+        Square.save_to_file([Square(1, 2)])
+        s = '[{"id": 20, "x": 2, "y": 0, "size": 1}]'
+        with open("Square.json") as f:
+            string_read = f.read()
+            self.assertEqual(string_read, s)
+        # test if load_from_file works on non existent files
+        myfile = "Square.json"
+        if os.path.isfile(myfile):
+            os.remove(myfile)
+        self.assertEqual(Square.load_from_file(), [])
+        # load one rectangle and test whether it's well saved n loaded
+        Square.save_to_file([Square(1, 2)])
+        self.assertEqual(
+            [str(i) for i in Square.load_from_file()],
+            ['[Square] (21) 2/0 - 1'])
+
+    def test_none(self):
+        """Temp test to see issues"""
+        Square.save_to_file(None)
+        with open("Square.json") as f:
+            string_read = f.read()
+            self.assertEqual(string_read, '[]')
 
 if __name__ == "__main___":
     unittest.main()
