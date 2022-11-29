03bae7e96dbc4b7c144735a94ec048a0458de6a6
Emmanuel Bongwe <50595883+BongweKE@users.noreply.github.com>
2022-11-29 21:44:28 +0300

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 --  a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 -- You are not allowed to use the DESCRIBE or EXPLAIN statements
-SHOW COLUMNS FROM `first_table`;
+SHOW FULL COLUMNS FROM `first_table`;
