8e1be7ad8b7519fdf74fe40f23abdbc6d97ae25d
Emmanuel Bongwe <50595883+BongweKE@users.noreply.github.com>
2022-11-30 21:19:12 +0300

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     PRIMARY KEY (`id`),
     id          INT           AUTO_INCREMENT,
-    states_id   INT           NOT NULL,
+    state_id   INT           NOT NULL,
     name        VARCHAR(256)  NOT NULL,
     FOREIGN KEY (`states_id`)
         REFERENCES `states`(`id`)
