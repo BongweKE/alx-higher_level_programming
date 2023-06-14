48f16885d2cf24c9c984ddcd48b636844349da70
BongweKE <bongwe.obi@gmail.com>
2023-06-14 07:59:16 +0000

    Number conversion using closures

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.converter = function (base) {
-
+  // create a closure to hold the base
+  return function (x) {
+    // the closure will this part
+    return x.toString(base);
+  };
 };
