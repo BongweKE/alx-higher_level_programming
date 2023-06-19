17fd889d4ccb084e316942dafa61532d754429d1
BongweKE <bongwe.obi@gmail.com>
2023-06-19 13:21:32 +0000

    Add SQLAlchemy tasks 6 n 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""7-model_state_fetch_all.py
+a script that lists all State objects from the database hbtn_0e_6_usa/
+
+Your script should take 3 arguments: mysql username, mysql password
+and database name/
+You must use the module SQLAlchemy/
+You must import State and Base from model_state/
+-from model_state import Base, State/
+Your script should connect to a MySQL server running on localhost at port 3306/
+Results must be sorted in ascending order by states.id/
+The results must be displayed as they are in the example below
+Your code should not be executed when imported
+"""
+import sys
+from model_state import (
+    Base,
+    State,
+)
+from sqlalchemy import (
+    engine,
+    create_engine,
+    select
+)
+
+def main():
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        f"mysql+mysqldb://{uname}:{pwd}@localhost:3306/{db}"
+    )
+    stmt = select(State).order_by(State.id.asc())
+    with engine.connect() as conn:
+        for row in conn.execute(stmt):
+            print(f"{row[0]}: {row[1]}")
+
+if __name__ == "__main__":
+    main()
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+a python file that contains the class definition of a State and
+an instance Base = declarative_base():
+
+State class:
+inherits from Base Tips
+links to the MySQL table states
+class attribute id that represents a column of an auto-generated, unique integer, can’t be null and is a primary key
+class attribute name that represents a column of a string with maximum 128 characters and can’t be null
+You must use the module SQLAlchemy
+Your script should connect to a MySQL server running on localhost at port 3306
+WARNING:
+all classes who inherit from Base must be imported before
+calling Base.metadata.create_all(engine)
+"""
+import sys
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    MetaData,
+    Sequence,
+    create_engine,
+)
+from sqlalchemy.orm import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(
+        Integer,
+        Sequence('state_id_seq'),
+        primary_key=True,
+        nullable=False
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        f"mysql+mysqldb://{uname}:{pwd}@localhost:3306/{db}"
+    )
+    Base.metadata.create_all(engine)
